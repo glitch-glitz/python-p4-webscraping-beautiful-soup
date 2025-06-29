9e90e7ab5a5ec28d0b7fd710f4c1f0435094c86d
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-06-29 07:52:50 +0300

    Add scraper script for Flatiron School site

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,18 @@
-from turtle import ht
+# from turtle import ht
+# from bs4 import BeautifulSoup
+# import requests
+
+# headers = {'user-agent': 'my-app/0.0.1'}
+# html = requests.get("https://flatironschool.com/", headers=headers)
 from bs4 import BeautifulSoup
 import requests
 
-headers = {'user-agent': 'my-app/0.0.1'}
+headers = {"user-agent": "my-app/0.0.1"}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, "html.parser")
+
+headings = doc.select(".heading-financier")
+
+for heading in headings:
+    print(heading.get_text(strip=True))
